9cf9e9bd77f3c3ebaead6a3c11049fc0426e204a
omerZeli <omer.zelinger@gmail.com>
2026-06-15 14:37:41 +0300

    fix deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -48,7 +48,7 @@ app.patch('/api/matches/:id/watched', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
 });
 
